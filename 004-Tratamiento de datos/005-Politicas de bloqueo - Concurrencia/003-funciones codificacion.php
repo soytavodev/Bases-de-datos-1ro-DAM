b40698d47b569324d0d1444265eebdec33f1967f
soytavodev <gustavoedv35@gmail.com>
2025-12-10 10:43:23 +0100

    1

---
+++ 004-Tratamiento de datos/005-Politicas de bloqueo - Concurrencia/003-funciones codificacion.php
@@ -0,0 +1,16 @@
+<?php
+	function codificar($cadena){
+  	for($i = 0;$i<9;$i++){
+    	$cadena = base64_encode($cadena);
+    }
+    return $cadena;
+  }
+  
+  $contrasena = "contraseñasegura1234";
+  echo $contrasena;
+  echo "<br>";
+  
+  echo codificar($contrasena);
+  echo "<br>";
+  
+?>
---
+++ 004-Tratamiento de datos/005-Politicas de bloqueo - Concurrencia/007-hasheo.php
@@ -0,0 +1,18 @@
+<?php
+
+	$cadena = "Hola";
+  echo $cadena;
+  echo "<br>";
+  
+  // Hash mediante md5
+  
+  $picadillo1 = md5($cadena);
+  echo $picadillo1;
+  echo "<br>";
+  
+  // Has mediante sha1
+  
+  $picadillo2 = sha1($cadena);
+  echo $picadillo2;
+  echo "<br>";
+?>
