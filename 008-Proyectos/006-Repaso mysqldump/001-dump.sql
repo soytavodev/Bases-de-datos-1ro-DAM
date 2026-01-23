6960f3c2d130aa653f9b96ab46cc666b90549956
soytavodev <gustavoedv35@gmail.com>
2026-01-23 08:50:16 +0100

    1

---
+++ 008-Proyectos/006-Repaso mysqldump/001-dump.sql
@@ -0,0 +1,8 @@
+sudo mysql -u root -p
+
+SHOW DATABASES;
+
+Una vez que conozco el nombre de la base de datos
+Me voy a la terminal y escribo:
+
+sudo mysqldump -u root -p blog2526 > backupblog2526.sql
---
+++ 008-Proyectos/006-Repaso mysqldump/004-escribe.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+archivo = open("lineas.txt",'a')
+archivo.write("Esta es una linea\n")
+archivo.close()
